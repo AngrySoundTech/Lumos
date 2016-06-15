ef3b09fe2534a2fca058d88a78ba897e096c43b7
Nicholas <nickcfeldman@gmail.com>
2016-06-15 18:09:05 -0400

    Update modlist and rename some jars

--- modlist.rb
+++ modlist.rb
@@ -1,18 +1,32 @@
-mods = Dir["src/mods/**/*.jar"]
+##
+# A simple script to generate a list of mods from jars in the directory
+# NOTE: This is not meant to be a perfect list until jars are named consitantly
+##
+def getModsList
+  mods = Dir["src/mods/**/*.jar"]
+  mods.each { |mod|
+    # Remove path
+    mod.gsub! /src\/mods\//, ''
+    mod.gsub! /1\.7\.10\//, ''
+    mod.gsub! /\.jar/, ''
 
-mods.each { |mod|
-  # Remove path
-  mod.gsub! /src\/mods\//, ''
-  mod.gsub! /1\.7\.10\//, ''
-  mod.gsub! /\.jar/, ''
+    # Remove Version Numbers
+    mod.gsub! /(?:(\d+)\.)?(?:(\d+)\.)?(\*|\d+)/, ''
 
-  # Remove Version Numbers
-  mod.gsub! /(?:(\d+)\.)?(?:(\d+)\.)?(\*|\d+)/, ''
+    # Remove tags
+    mod.gsub! /\[.*\]/, ''
 
-  # Remove biddly bobs
-  mod.gsub! /\s/, ''
-  mod.gsub! /-|\.|_/, ''
-  mod.gsub! /\(\)/, ''
-}
+    # Remove biddly bobs
+    mod.gsub! /\s/, ''
+    mod.gsub! /-|\.|_/, ''
+    mod.gsub! /\(\)/, ''
 
-puts mods
+    mod[0] = mod[0].upcase
+  }
+
+  mods.sort
+end
+
+if __FILE__ == $0
+    puts getModsList
+end
